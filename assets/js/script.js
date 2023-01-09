db22c6f3992c83eb609074ddf014ff45b22fc69b
Tomislav Dukez <tomdu3@ymail.com>
2023-01-09 20:47:28 +0000

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,12 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === 'multiply') { 
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        // if (num1 < num2) {
+        //     [num1, num2] = [num2, num1];
+        // }    // this is how would I solve the issue of non negative results
+
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -69,6 +75,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else {
         alert(`Unimplemented operator $(operator)`);
         throw `Unimplemented operator $(operator). Aborting!`;
@@ -100,8 +108,12 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
-    
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    // document.getElementById('operand1').textContent = operand1;
+    // document.getElementById('operand2').textContent = operand2; // this would work in case of my solution above
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
